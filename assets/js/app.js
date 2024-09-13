b043757878e5c2098f9eaa36234b9e57f3489790
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-13 16:29:19 +0400

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+import { showError } from "./helper.js";
+import dom from "./selectors.js";
+import { getMenu } from "./service.js";
+import renderProduct from "./ui.js";
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const menu = await getMenu();
+
+  if (menu && menu.length > 0) {
+    renderProduct(menu);
+  } else {
+    showError("No menu items available", "block");
+  }
+});
