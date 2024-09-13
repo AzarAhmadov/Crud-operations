208438a07ebce70fb60d46c82f9c3a41901c07fa
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-13 16:28:37 +0400

    selects DOM elements

---
+++ assets/js/selectors.js
@@ -0,0 +1,9 @@
+const dom = {
+  products: document.querySelector("#products"),
+  table_body: document.querySelector("#table_body"),
+  error: document.querySelector("#error-message"),
+  delete_modal: document.querySelector(".delete_modal"),
+  modal_control: document.querySelector("#modal_control"),
+};
+
+export default dom;
