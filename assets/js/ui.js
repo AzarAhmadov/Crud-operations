e632cd8515e07b3b784f52c4db1656a52449ea24
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-14 20:15:30 +0400

    Add animate.css

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -5,7 +5,7 @@ const renderProduct = (data) => {
 
   for (let item of data) {
     html += `
-    <tr class="bg-white hover:bg-[#f0f3f4] transition-all">
+    <tr class="bg-white hover:bg-[#f0f3f4] animate__animated animate__zoomIn">
         <td class="pl-6 py-4">
           <img
             class="size-[3rem] md:size-[5rem] rounded-md object-cover"
@@ -17,7 +17,7 @@ const renderProduct = (data) => {
         <td class="px-6 py-4">${item.category}</td>
         <td class="px-6 py-4">${item.price}</td>
         <td class="px-6 py-4 space-x-2 text-center">
-          <button class="bg-green-500 text-sm hover:bg-green-700 text-white py-2 px-5 rounded-md">
+          <button onclick="getMenuById(${item.id})" class="bg-green-500 text-sm hover:bg-green-700 text-white py-2 px-5 rounded-md">
             Edit
           </button>
           <button 
