242fff7b97cc5522a7853457cbf1efb5fd9a0b4a
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-14 19:54:03 +0400

    Update validation.js

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,9 +1,11 @@
-import { removeErrorMsg } from "./helper.js";
+import { allowOnlyNumbers, removeErrorMsg } from "./helper.js";
 
 const formValidation = (form) => {
     const value = form.value;
     const data_required = form.dataset.required === "true";
     const data_https = form.dataset.https;
+    const data_del = form.dataset.tel
+
     removeErrorMsg(form);
     let isValid = true
 
@@ -15,14 +17,23 @@ const formValidation = (form) => {
         );
         isValid = false
 
-    } else if (value.trim() && data_https && !value.trim().startsWith(data_https)) {
+    }
+    else if (value.trim() && data_https && !value.trim().startsWith(data_https)) {
         form.closest("div").insertAdjacentHTML(
             "beforeend",
             `<p class="error-el mt-2 text-[#F22B2B] text-[13px]">Please enter a valid URL starting with ${data_https}</p>`
         );
         isValid = false
 
-    } else {
+    }
+
+    else if (value.trim() && data_del && !allowOnlyNumbers(value)) {
+        form.closest("div").insertAdjacentHTML(
+            "beforeend",
+            `<p class="error-el mt-2 text-[#F22B2B] text-[13px]">Only the number can be entered</p>`
+        );
+    }
+    else {
         form.classList.remove("!border-red-500");
     }
 
