5402f12673da0b8be2f9e5cea9e25e3954a14b26
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-14 13:14:46 +0400

    Update selectors.js

--- assets/js/selectors.js
+++ assets/js/selectors.js
@@ -3,7 +3,9 @@ const dom = {
   table_body: document.querySelector("#table_body"),
   error: document.querySelector("#error-message"),
   delete_modal: document.querySelector(".delete_modal"),
+  add_modal: document.querySelector(".add_modal"),
   modal_control: document.querySelector("#modal_control"),
+  add_product_form: document.querySelector("#add_product_form"),
 };
 
 export default dom;
