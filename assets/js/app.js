6ff0f171d5ded75c3696e2d7aeb7e2637eebbcbe
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-14 16:47:16 +0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
-import { showAddModal, showError } from "./helper.js";
+import { showError } from "./helper.js";
 import dom from "./selectors.js";
 import { getMenu } from "./service.js";
 import renderProduct from "./ui.js";
+import formValidation from "./validation.js";
 
 window.addEventListener("DOMContentLoaded", async () => {
   const menu = await getMenu();
@@ -9,6 +10,27 @@ window.addEventListener("DOMContentLoaded", async () => {
   if (menu && menu.length > 0) {
     renderProduct(menu);
   } else {
-    showError("No menu items available", "block");
+    showError("No menu items available", "flex");
   }
+
+  dom.add_menu_form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    let allValid = true
+    const formData = {};
+
+    for (let input of e.target.elements) {
+      if (input.tagName !== "BUTTON") {
+        const isValid = formValidation(input)
+        if (!isValid) {
+          allValid = false
+        } else {
+          formData[input.name] = input.value
+        }
+      }
+    }
+
+    if (allValid) {
+      await window.addMenu(formData)
+    }
+  });
 });
