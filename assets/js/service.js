1f87ef3ac2442d23a76416d1a3846683461dfbe9
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-15 00:05:47 +0400

    Add post and put service

--- assets/js/service.js
+++ assets/js/service.js
@@ -1,5 +1,5 @@
 import { API, BASE_URL } from "./api.js";
-import { showDeleteModal } from "./helper.js";
+import { fillForm, showDeleteModal } from "./helper.js";
 import renderProduct from "./ui.js";
 
 const getMenu = async () => {
@@ -10,6 +10,30 @@ const getMenu = async () => {
     }
     const data = await res.json();
     return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+window.addMenu = async (data) => {
+  try {
+
+    data.id = Math.floor(10000 + Math.random() * 90000);
+
+    const res = await fetch(`${BASE_URL}/${API.post}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+      throw new Error(`Error ${res.status} ${res.statusText}`);
+    }
+
+    const updatedMenu = await getMenu();
+    renderProduct(updatedMenu.reverse());
   } catch (err) {
     console.log(err);
   }
@@ -40,12 +64,47 @@ const deleteMenuById = async (id) => {
     if (!res.ok) {
       throw new Error(`Error ${res.status} ${res.statusText}`);
     }
-
     const menu = await getMenu();
     renderProduct(menu);
   } catch (err) {
     console.log(err);
   }
 };
+
+window.updateMenu = async (data) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${API.menu}/${data.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+      throw new Error(`Error ${res.status} ${res.statusText}`);
+    }
+
+    const updatedMenu = await getMenu();
+    renderProduct(updatedMenu.reverse());
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+window.getMenuById = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${API.menuById.replace(':id', id)}`);
+    if (!res.ok) {
+      throw new Error(`Error ${res.status} ${res.statusText}`);
+    }
+
+    OpenModal('Update menu', "Update")
+    const data = await res.json();
+    fillForm(data)
+
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 
 export { getMenu };
